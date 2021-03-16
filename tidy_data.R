7a94e43ceb1016ed7f90d13d344f96d8b649beeb
MATT JOHNSON <johnson.matt1818@gmail.com>
2021-03-16 13:46:53 -0400

    Fixed corrupted emails Q1

--- tidy_data.R
+++ tidy_data.R
@@ -52,5 +52,68 @@ Inbox.Final <- left_join(DF.From.To.raw2, users) %>%
 
 length(unique(Inbox.Final$to.mailname))
 
-write.csv(Inbox.Final, "derived_data/Inbox.From.Any.To.User.csv")
 #########################################################################################################
+
+#ok, lets try ot fix the ones that look like this, "houston <.ward@", and "pr <.palmer@"
+# it seems like they all look the same so we should be able to remove the error with the 
+# @ and <
+
+#seems to be in both sender and receiver and sometimes both, lets fix that
+
+#lets deal with the both case first
+inbox.both.issues <- Inbox.Final %>% 
+  filter(grepl("<", to.mailname) & grepl("<", from.mailname)) %>% 
+  #separate in half to remove the space and <
+  separate(to.mailname, into = c("to.mailname.first", "to.mailname.last"), sep = " <") %>% 
+  #remove the @ sign in the lastname
+  separate(to.mailname.last, into = c("to.mailname.last", "Nothing"), sep = "@") %>% 
+  #remove nothing column
+  select(-Nothing) %>% 
+  #rejoin name and last name
+  unite("to.mailname", to.mailname.first:to.mailname.last, sep = "") %>% 
+  #separate in half to remove the space and <
+  separate(from.mailname, into = c("from.mailname.first", "from.mailname.last"), sep = " <") %>% 
+  #remove the @ sign in the lastname
+  separate(from.mailname.last, into = c("from.mailname.last", "Nothing"), sep = "@") %>% 
+  #remove nothing column
+  select(-Nothing) %>% 
+  #rejoin name and last name
+  unite("from.mailname", from.mailname.first:from.mailname.last, sep = "")
+#now before I do the next step I have to join this back in
+# and make a temp file to the rest of it can run on the set without
+# the errors on both sides
+inbox.from.to.temp1 <- Inbox.Final %>% 
+  filter(!(grepl("<", from.mailname) & grepl("<", to.mailname)))
+inbox.clean.temp <- rbind(inbox.from.to.temp1, inbox.both.issues)
+
+inbox.to.issues <- inbox.clean.temp %>% 
+  filter(grepl("<", to.mailname)) %>% 
+  #separate in half to remove the space and <
+  separate(to.mailname, into = c("to.mailname.first", "to.mailname.last"), sep = " <") %>% 
+  #remove the @ sign in the lastname
+  separate(to.mailname.last, into = c("to.mailname.last", "Nothing"), sep = "@") %>% 
+  #remove nothing column
+  select(-Nothing) %>% 
+  #rejoin name and last name
+  unite("to.mailname", to.mailname.first:to.mailname.last, sep = "")
+
+inbox.from.issues <- inbox.clean.temp %>% 
+  #ok found a really odd outlier I am going to remove all together first
+  filter(!grepl("@enron@", from.mailname)) %>% 
+  filter(grepl("<", from.mailname)) %>% 
+  #separate in half to remove the space and <
+  separate(from.mailname, into = c("from.mailname.first", "from.mailname.last"), sep = " <") %>% 
+  #remove the @ sign in the lastname
+  separate(from.mailname.last, into = c("from.mailname.last", "Nothing"), sep = "@") %>% 
+  #remove nothing column
+  select(-Nothing) %>% 
+  #rejoin name and last name
+  unite("from.mailname", from.mailname.first:from.mailname.last, sep = "")
+
+#ok, now lets remove them from the original then rbind with these new ones
+inbox.from.to.temp2 <- inbox.clean.temp %>% 
+  filter(!grepl("<", from.mailname) & !grepl("<", to.mailname))
+
+inbox.clean.final <- rbind(inbox.from.to.temp2, inbox.from.issues, inbox.to.issues)
+
+write.csv(inbox.clean.final, "derived_data/Inbox.From.Any.To.User.csv")
