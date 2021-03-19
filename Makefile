4064295c1871211bc31e74d3607e9e8664ac2124
MATT JOHNSON <johnson.matt1818@gmail.com>
2021-03-19 15:29:06 -0400

    Fixed some things

--- Makefile
+++ Makefile
@@ -10,18 +10,22 @@ Analysis.pdf:\
  Analysis.Rmd
 	R -e "rmarkdown::render('Analysis.Rmd')"
 	
-
+# Network Plots
+derived_graphics/isolated.network.spectral.comunity.png\
+derived_graphics/top.100.network.spectral.comunity.png\
 README_graphics/top.100.network.png:\
  derived_data/Inbox.Outbox.csv\
  tidy_network_plot.R
 	Rscript tidy_network_plot.R
 
+# Barplots
 README_graphics/Top.30.Send.Receive.Plot.png\
 derived_graphics/Top.30.Send.Receive.Plot.png:\
  derived_data/Inbox.Outbox.csv\
  tidy_graphics.R
 	Rscript tidy_graphics.R
 
+# Data
 derived_data/Inbox.Of.User.csv\
 derived_data/Sent.By.User.csv\
 derived_data/Inbox.Outbox.csv:\
--- tidy_network_plot.R
+++ tidy_network_plot.R
@@ -1,6 +1,7 @@
 ### load libs and data
 library(igraph)
 library(tidyverse)
+set.seed(18)
 
 inbox.from.to <- read.csv("derived_data/Inbox.Outbox.csv") %>% 
   select(-X)
@@ -9,6 +10,8 @@ inbox.from.to <- read.csv("derived_data/Inbox.Outbox.csv") %>%
 #this many users total sending emails to our users
 
 #what would happen if a did a groupby for both to and from and did a count?
+inbox.from.to$to.mailname <- str_trim(inbox.from.to$to.mailname)
+inbox.from.to$from.mailname <- str_trim(inbox.from.to$from.mailname)
 
 from.any.to.any.count <- tibble(inbox.from.to) %>% 
   filter(from.mailname != "40enron" & to.mailname != "40enron") %>% 
@@ -40,16 +43,72 @@ edges1 <- edges %>%
   select(from, to, Weight)
 
 Network_igraph <- graph_from_data_frame(d = edges1, vertices = nodes, directed = T)
-# radian.rescale <- function(x, start=0, direction=1) {
-#   c.rotate <- function(x) (x + start) %% (2 * pi) * direction
-#   c.rotate(scales::rescale(x, c(0, 2 * pi), range(x)))
-# }
-# n <- 100
-# lab.locs <- radian.rescale(x=1:n, direction=-1, start=0)
 
-png("README_graphics/top.100.network.png", width = 2000, height = 2000)
+#plot using node$id as vertex labels, can look back later to see who is who
+
+#circle Plot
+png("README_graphics/top.100.network.circle.png", width = 2000, height = 2000)
 plot.igraph(Network_igraph, vertex.size = 4, layout = layout_in_circle, 
             edge.color = edges1$Weight, edge.width = 1/15*edges$Weight, edge.curved = T, 
             vertex.label = nodes$id)
 title("Top 100 Employee Email Count Network", cex.main = 6, col.main="black")
+dev.off()
+
+#community detection
+
+Network_igraph.non.direc <- graph_from_data_frame(d = edges1, vertices = nodes, directed = F)
+Spectral.comun <- cluster_leading_eigen(Network_igraph.non.direc)
+
+#graphopt with colored vertex for communities
+png("derived_graphics/top.100.network.spectral.comunity.png", width = 2000, height = 2000)
+plot(Network_igraph.non.direc, layout = layout_with_graphopt, vertex.size = 4, 
+     layout = layout_with_graphopt, edge.color = "gray", edge.curved = T, 
+     edge.width = 1/50*edges$Weight, vertex.label = nodes$label, 
+     vertex.color = membership(Spectral.comun))
+dev.off()
+
+############################################ Isolated Plot ###################################
+
+#now lets try to isolate this large group in the middle, 
+#the rest seem a bit detached from others, but this group 
+#in the middle seemed to be talking to everyone
+
+#plus one of them the main people there is tim belden
+
+#what if I just look at the larger plot and check out the names of people in here
+
+DF.isolate <- from.any.to.any.count %>% 
+  filter(Destination == "tim.belden" | Destination == "holden.salisbury" | 
+           Source == "grace.rodriguez" | Source == "heather.dunton" | Destination == "jeff.dasovich" |
+           Source == "d..steffes" | Destination == "paul.kaufman" | Source == "j..kean" |
+           Destination == "joseph.alamo" | Destination == "j..kean" | Destination == "d..steffes" |
+           Destination == "richard.shapiro" | Source == "jeff.dasovich") %>% 
+  arrange(Source)
+
+nodes <- tibble(
+  "id" = 1:length(unique(c(DF.isolate$Source, DF.isolate$Destination))),
+  "label" = unique(c(DF.isolate$Source, DF.isolate$Destination))
+)
+sent.recieve <- tibble(DF.isolate)
+
+edges <- sent.recieve %>% 
+  left_join(nodes, by = c("Source" = "label")) %>% 
+  rename(from = id)
+
+edges <- edges %>% 
+  left_join(nodes, by = c("Destination" = "label")) %>% 
+  rename(to = id)
+
+edges1 <- edges %>% 
+  select(from, to, Weight)
+
+Network_igraph.iso <- graph_from_data_frame(d = edges1, vertices = nodes, directed = F)
+
+Spectral.comun.iso <- cluster_leading_eigen(Network_igraph.iso)
+
+png("derived_graphics/isolated.network.spectral.comunity.png", width = 2000, height = 2000)
+plot(Network_igraph.iso, layout = layout_with_graphopt, vertex.size = 4, 
+     layout = layout_with_graphopt, edge.color = "black", edge.curved = T, 
+     edge.width = 1/25*edges1$Weight, vertex.label = nodes$label, vertex.color = membership(Spectral.comun.iso), 
+     vertex.label.cex = 3, edge.label = edges1$Weight, edge.label.cex = 3)
 dev.off()
